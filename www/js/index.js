e36f0d28ef800699280c25e693611abb80e94221
josue munoz <josue@45graphics.net>
2017-01-04 00:29:00 -0800

    js

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ar app = {
+	
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -33,17 +34,101 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        console.log('Received Device Ready Event');
+        console.log('calling setup push');
+        app.setupPush();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	
+    setupPush: function() {
+        console.log('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                //"senderID": "eElVSOVofa8"
+				 "senderID": 71408143043
+				//eElVSOVofa8
+            },
+            "browser": {},
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        console.log('after init');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        push.on('registration', function(data) {
+            console.log('registration event: ' + data.registrationId);
+			//localStorage.setItem('registrationId', data.registrationId);
+//alert(data.registrationId);
+            var oldRegId = localStorage.getItem('registrationId');
+			//localStorage.setItem('registrationId', data.registrationId);
+			
+			// alert(data.registrationId);
+            
+			var appName = "3dlogodesign";
+ 			var appCategory = "pushNotifications";
+			var d = new Date();
+			var appRegistration = d.getFullYear()+"-"+ d.getMonth() +"-"+ d.getDate();
+ 			//var appUserName = prompt("your name", "");
+			
+				
+			//if (oldRegId) {
+			if(oldRegId !== data.registrationId) { //working one
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+               
+				// Post registrationId to your app server as the value has changed
+				
+				x = new XMLHttpRequest();
+				//alert(device.model+ " " +device.uuid +" "+device.platform)
+				var sendData  = "?regid="+data.registrationId;
+					sendData += "&appName="+escape(appName);
+					sendData += "&appCategory="+escape(appCategory);
+					sendData += "&appRegistration="+escape(appRegistration);
+					sendData += "&deviceModel="+escape(device.model);
+					sendData += "&uuid="+escape(device.uuid);
+					sendData += "&devicePlatform="+escape(device.platform);
+					
+					
+				x.open("GET", "http://45graphics.net/curlTest/index.php"+sendData, true);
+				x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+				x.onreadystatechange = function(){
+						if(x.readyState == 4 && x.status == 200){
+							//alert(x.responseText);
+						}
+				}
+		
+			x.send();
+				}
 
-        console.log('Received Event: ' + id);
+            var parentElement = document.getElementById('registration');
+            var listeningElement = parentElement.querySelector('.waiting');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+        });
+
+        push.on('error', function(e) {
+            console.log("push error = " + e.message);
+        });
+	
+
+        push.on('notification', function(data) {
+            //console.log('notification event');
+			//var ul = document.getElementById("targetbaby");
+			//var li = document.createElement("li");
+				//li.innerHTML =  data.title+" "+data.message;
+				//ul.appendChild(li); 
+            navigator.notification.alert(
+                data.message,          // message
+                null,                 // callback
+               data.title,           // title
+				'Ok'                // buttonName
+            );
+			
+       });
     }
 };
+
